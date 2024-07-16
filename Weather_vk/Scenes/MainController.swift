f45c9242de84872e1c4ddebded6e6d6ab3f82afc
Anatolii Shumov <seminarsds@gmail.com>
2024-07-16 15:42:24 +0200

    initial commit

---
+++ Weather_vk/Scenes/MainController.swift
@@ -0,0 +1,20 @@
+//
+//  MainController.swift
+//  Weather_vk
+//
+//  Created by Anatolii Shumov on 16.07.2024.
+//
+
+import UIKit
+
+class MainController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+
+
+}
+
